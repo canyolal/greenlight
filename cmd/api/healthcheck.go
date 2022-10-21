c90f37e6861064919a1f14c25c541e745ec40ec8
Can Yolal <canyolal16@gmail.com>
2022-10-21 11:27:26 +0300

    json.Marshal used to transform Go obj. to JSON

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,16 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 
-	js := `{"status": "available" , "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "server encountered a problem and couldn't process the request", http.StatusInternalServerError)
+		return
+	}
+
+	js = append(js, '\n')
 
 	w.Header().Set("Content-Type", "application/json")
 
-	w.Write([]byte(js))
+	w.Write(js)
 }
