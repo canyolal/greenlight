fd2b389fae4e14fa4c6e6cb24bd5afcd19caccad
Can Yolal <canyolal16@gmail.com>
2022-10-21 14:33:34 +0300

    bugfix - error args removed from notFoundResponse and methodNotAllowedResponse methods

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -33,7 +33,7 @@ func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Reque
 
 // The notFoundResponse() method will be used to send a 404 Not Found status code and
 // JSON response to the client.
-func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
 
 	message := "the requested resource could not be found"
 	app.errorResponse(w, r, http.StatusNotFound, message)
@@ -41,7 +41,7 @@ func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request,
 
 // The methodNotAllowedResponse() method will be used to send a 405 Method Not Allowed
 // status code and JSON response to the client.
-func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request, err error) {
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
 
 	message := fmt.Sprintf("the %s method is not suppeorted for this resource", r.Method)
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -16,7 +16,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	id, err := app.readIDParam(r)
 	if err != nil {
-		app.notFoundResponse(w, r, err)
+		app.notFoundResponse(w, r)
 		return
 	}
 
