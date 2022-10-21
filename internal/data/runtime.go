9f63031bac270ae182709724044b2cd77ef026b6
Can Yolal <canyolal16@gmail.com>
2022-10-21 14:02:06 +0300

    Movie struct's runtime field is changed to Runtime type to support unique MarshalJSON() method

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -6,11 +6,11 @@ import (
 )
 
 // Custom runtime type has underlying type int32
-type runtime int32
+type Runtime int32
 
 // MarshalJSON() method for runtime type which satisfies Marshaler interface so that
 // runtime can return in "r mins" format in JSON.
-func (r *runtime) MarshalJSON() ([]byte, error) {
+func (r *Runtime) MarshalJSON() ([]byte, error) {
 	jsonVal := fmt.Sprintf("%d mins", r)
 
 	// Use the strconv.Quote() function on the string to wrap it in double quotes. It
