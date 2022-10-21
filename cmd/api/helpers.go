39f314cfbfadde0c9c4c901310903f69b6d09ed2
Can Yolal <canyolal16@gmail.com>
2022-10-21 20:09:36 +0300

    badRequestResponse() helper

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -102,3 +102,7 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 	}
 	return nil
 }
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
