94f4b81c385bea56173ff0418b68e311167b26a7
Can Yolal <canyolal16@gmail.com>
2022-10-25 21:13:23 +0300

    updateMovieHandler() HTTP Method Put is changed to Patch

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -107,11 +107,12 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 
 	}
 
+	// Use pointers for the Title, Year and Runtime fields.
 	var input struct {
-		Title   string       `json:"title"`
-		Year    int32        `json:"year"`
-		Runtime data.Runtime `json:"runtime"`
-		Genres  []string     `json:"genres"`
+		Title   *string       `json:"title"`
+		Year    *int32        `json:"year"`
+		Runtime *data.Runtime `json:"runtime"`
+		Genres  []string      `json:"genres"`
 	}
 
 	err = app.readJSON(w, r, &input)
@@ -120,10 +121,19 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Runtime = input.Runtime
-	movie.Genres = input.Genres
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+	// Note that we don't need to dereference a slice.
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
 
 	v := validator.New()
 
@@ -144,6 +154,7 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 }
 
+// Deletes a movie from DB for given id.
 func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Request) {
 
 	id, err := app.readIDParam(r)
