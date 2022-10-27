1351be9b9f31d2729b8b7db44e5fa8375a6f4678
Can Yolal <canyolal16@gmail.com>
2022-10-27 11:48:10 +0300

    Panic Recovering returns 500 Int. Srv. Err.

---
+++ cmd/api/middleware.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create a deferred function (which will always be run in the event of a panic
+		// as Go unwinds the stack).
+		defer func() {
+			if err := recover(); err != nil {
+				// Use the builtin recover function to check if there has been a panic or
+				// not.
+				w.Header().Set("Connection", "lost")
+				// The value returned by recover() has the type interface{}, so we use
+				// fmt.Errorf() to normalize it into an error and call our
+				// serverErrorResponse() helper. In turn, this will log the error using
+				// our custom Logger type at the ERROR level and send the client a 500
+				// Internal Server Error response.
+				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
