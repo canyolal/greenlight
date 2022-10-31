d11e31a7d5af69f2ea9afbdcfdc1fbc5181d8ffd
Can Yolal <canyolal16@gmail.com>
2022-10-31 12:57:15 +0300

    SMTP mailer for registerUserHandler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/canyolal/greenlight/internal/data"
 	"github.com/canyolal/greenlight/internal/jsonlog"
+	"github.com/canyolal/greenlight/internal/mailer"
 	_ "github.com/lib/pq"
 )
 
@@ -28,12 +29,20 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
@@ -52,6 +61,12 @@ func main() {
 	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum request per second")
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 587, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", os.Getenv("SMTP_username"), "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", os.Getenv("SMTP_password"), "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.canyolal.com>", "SMTP sender")
 
 	flag.Parse()
 
@@ -72,6 +87,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
--- go.mod
+++ go.mod
@@ -9,3 +9,8 @@ require github.com/lib/pq v1.10.0
 require golang.org/x/time v0.1.0
 
 require golang.org/x/crypto v0.1.0
+
+require (
+	github.com/go-mail/mail/v2 v2.3.0
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc
+)
