b446b36e9a501cb5da6c7f9f5a829c8cdb6dcf87
Can Yolal <canyolal16@gmail.com>
2022-10-31 13:06:47 +0300

    background() goroutine helper added

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -185,3 +185,18 @@ func (app *application) readInt(qs url.Values, key string, defaultValue int, v *
 
 	return i
 }
+
+// Background helper accepts an arbitrary function as a parameter
+func (app *application) background(fn func()) {
+	// Launch a background goroutine.
+	go func() {
+		// Recover any panic.
+		defer func() {
+			if err := recover(); err != nil {
+				app.logger.PrintError(fmt.Errorf("%s", err), nil)
+			}
+		}()
+		// Execute the arbitrary function that has passed as parameter.
+		fn()
+	}()
+}
