5b058fa2b746669d3eda7d246ce11cc72e568ba0
Can Yolal <canyolal16@gmail.com>
2022-11-04 21:00:24 +0300

    Password-Reset logic and prod. deployment

--- cmd/api/tokens.go
+++ cmd/api/tokens.go
@@ -64,3 +64,65 @@ func (app *application) createAuthentictaionTokenHandler(w http.ResponseWriter, 
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *application) createPasswordResetTokenHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Email string `json:"email"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	data.ValidateEmail(v, input.Email)
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	user, err := app.models.Users.GetByEmail(input.Email)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			v.AddError("email", "no matching email found")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	if !user.Activated {
+		v.AddError("email", "user account must be activated")
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	token, err := app.models.Tokens.New(user.Id, 30*time.Minute, data.ScopePasswordReset)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	app.background(func() {
+		data := map[string]interface{}{
+			"resetToken": token.Plaintext,
+		}
+		err = app.mailer.Send(user.Email, "password_reset.tmpl", data)
+		if err != nil {
+			app.logger.PrintError(err, nil)
+		}
+	})
+
+	respMsg := envelope{"message": "email is sent to you with reset instructions"}
+
+	err = app.writeJSON(w, http.StatusAccepted, respMsg, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
